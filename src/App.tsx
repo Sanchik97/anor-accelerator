f3430bd0a117278d13e0d0ddb9d49c14873c6527
Sanjar Fayziyev <s.fayziyev@hlsys.uz>
2026-07-07 19:33:55 +0500

    Added husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,39 @@
 import '@mantine/core/styles.css'
 import '@mantine/dates/styles.css'
-import "@mantine/notifications/styles.css"
+import '@mantine/notifications/styles.css'
 
 import './App.css'
-import {createTheme, MantineProvider} from "@mantine/core";
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
-import {createBrowserRouter, RouterProvider} from "react-router";
-import {HomePage} from "./pages/HomePage.tsx";
-import {NotFoundPage} from "./pages/NotFoundPage.tsx";
+import { createTheme, MantineProvider } from '@mantine/core'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import { HomePage } from './pages/HomePage.tsx'
+import { NotFoundPage } from './pages/NotFoundPage.tsx'
 
 const theme = createTheme({
-    primaryColor: 'red'
-});
+  primaryColor: 'red',
+})
 
 const client = new QueryClient()
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <HomePage/>
-    },
-    {
-        path: '*',
-        element: <NotFoundPage/>
-    }
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />,
+  },
 ])
 
 function App() {
-    return (
-        <QueryClientProvider client={client}>
-            <MantineProvider theme={theme}>
-                <RouterProvider router={router}/>
-            </MantineProvider>
-        </QueryClientProvider>
-    )
+  return (
+    <QueryClientProvider client={client}>
+      <MantineProvider theme={theme}>
+        <RouterProvider router={router} />
+      </MantineProvider>
+    </QueryClientProvider>
+  )
 }
 
 export default App
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,3 @@
 export function NotFoundPage() {
-    return (
-        <h1>Not Found</h1>
-    )
+  return <h1>Not Found</h1>
 }
